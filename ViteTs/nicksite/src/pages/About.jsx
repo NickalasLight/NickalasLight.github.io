4eabbcf676d7d56f6f312c94da39a03c1122ac04
NickalasLight <nickalas.93@gmail.com>
2024-11-17 16:03:12 +0100

    removing test code for checking deploy was working

--- ViteTs/nicksite/src/pages/About.jsx
+++ ViteTs/nicksite/src/pages/About.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 
 const About = () => <div>
-
     <p>Hello! This is my site. I am Nick, a hacker, coder, programmer, software engineer, fullstack, etc. etc. etc.
         whatever you are supposed to call it these days</p>
     <p>I decided to make a personal website and make a blog, I guess to follow the trend of every other guy that gets
