8f0665e6451b80f4d8ddf4a5fde4979013cad4df
NickalasLight <nickalas.93@gmail.com>
2024-11-17 16:14:24 +0100

    adding google analytics, having public id is prob bad, i'll fix in a sec

--- ViteTs/nicksite/vite.config.js
+++ ViteTs/nicksite/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginRadar from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+
+
+
+    VitePluginRadar({
+      analytics: {
+        id: 'G-1M1GVGYGH9', // Replace with your GA4 Measurement ID
+      },
+    }),
+
+
+
+
+  ],
 })
